674a45579792fcfcbf54c4393734b19c568392bd
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2024-05-08 01:38:48 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports={
     DB_NAME:"crm_db",
-    DB_URL:"mongodb+srv://nitesh:Ni8@te#9s@atlascluster.56wfbjt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
+    DB_URL:"mongodb+srv://nitesh:Ni8@te#9s@atlascluster.56wfbjt.mongodb.net/typing?retryWrites=true&w=majority&appName=AtlasCluster"
 }
